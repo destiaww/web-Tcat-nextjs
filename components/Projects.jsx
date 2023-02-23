af31b44529e2e84c03a5ac5d7e797b879ef139f1
destiaww <destiawlf@gmail.com>
2023-02-23 09:31:53 +0700

    fix piagam error

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,45 +1,32 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
-import boutImg from '../public/assets/projects/boutd.png';
-import daftarImg from '../public/assets/projects/pendaftaran-telkom.png'
-import piagamImg from '../public/assets/projects/piagam.jpg'
-import tdormImg from '../public/assets/projects/tdorm.png'
-import ProjectItem from './ProjectItem';
+import React from "react";
+import boutImg from "../public/assets/projects/boutd.png";
+import daftarImg from "../public/assets/projects/pendaftaran-telkom.png";
+import tdormImg from "../public/assets/projects/tdorm.png";
+import ProjectItem from "./ProjectItem";
 
 const Projects = () => {
   return (
-    <div id='projects' className='w-full'>
-      <div className='max-w-[1240px] mx-auto px-2 py-16'>
-
-        <h2 className='flex py-4 justify-center'>My Portofolio</h2>
-        <div className='grid md:grid-cols-2 gap-8'>
+    <div id="projects" className="w-full">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <h2 className="flex py-4 justify-center">My Portofolio</h2>
+        <div className="grid md:grid-cols-2 gap-8">
           <ProjectItem
-            title='boutD'
+            title="boutD"
             backgroundImg={boutImg}
-            projectUrl='/boutd'
-            tech='Next Js'
+            projectUrl="/boutd"
+            tech="Next Js"
           />
           <ProjectItem
-            title='web telkom'
+            title="web telkom"
             backgroundImg={daftarImg}
-            projectUrl='/pendaftaran-telkom'
-            tech='Html Css Js'
-
+            projectUrl="/pendaftaran-telkom"
+            tech="Html Css Js"
           />
           <ProjectItem
-            title='Piagam'
-            backgroundImg={piagamImg}
-            projectUrl='/piagam'
-            tech='Akademik'
-
-          />
-          <ProjectItem
-            title='T-Dorm'
+            title="T-Dorm"
             backgroundImg={tdormImg}
-            projectUrl='/tdorm'
-            tech='Figma'
-
+            projectUrl="/tdorm"
+            tech="Figma"
           />
         </div>
       </div>
